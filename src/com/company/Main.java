1cc177e45ffc903f3bd6a4c58575153c65196446
BT_1N3_17 <zfeng7527>
2019-01-17 08:35:31 -0500

    D:

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	double[] scan ={0.3,0.7,0.8,0.4,1.4,1.1,0.2,0.5,0.2,1.6,0.6,0.9};
-	double[] scan2={0.3,0.7,0.8,0.4,1.4,1.1};
+		double[] scan ={0.3,0.7,0.8,0.4,1.4,1.1,0.2,0.5,0.2,1.6,0.6,0.9};
+		double[] scan2={0.3,0.7,0.8,0.4,1.4,1.1};
 
-	SkyView sv1 = new SkyView(4,3,scan);
-	SkyView sv2 = new SkyView(3,2,scan2);
+		SkyView sv1 = new SkyView(4,3,scan);
+		System.out.println();
+		SkyView sv2 = new SkyView(3,2,scan2);
 
-	System.out.println(sv1);
-	System.out.println(sv2);
+		//System.out.println(sv1);
+		//System.out.println(sv2);
 
-	System.out.println("Average 1: "+sv1.getAverage(1,3,0,2));
-	System.out.println("Average 1: "+sv1.getAverage(0,1,0,1));
+		System.out.println();
+		System.out.println("Average 1: "+sv1.getAverage(1,3,0,2));
+		System.out.println("Average 1: "+sv1.getAverage(0,1,0,1));
     }
 }
